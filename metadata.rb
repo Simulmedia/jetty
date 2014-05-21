87905b12482f0e02bb74548cfb646ca7716c9e69
Adrien Brault <adrien.brault@gmail.com>
2014-05-20 21:19:50 -0700

    Allow centos/redhat/fedora

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,10 @@ license          "Apache 2.0"
 description      "Installs/Configures Jetty"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.9.0"
+
 supports 'ubuntu', ">= 12.04"
+%w{ centos redhat fedora }.each do |os|
+  supports os
+end
 
 depends "java", ">= 1.10.0"
