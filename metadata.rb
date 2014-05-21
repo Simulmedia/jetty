cd9fc3065d553b528e32c2e591b917dbd8535054
Adrien Brault <adrien.brault@gmail.com>
2014-05-20 22:27:59 -0700

    Fix encoding issue with ruby 2.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "HipSnip Limited"
 maintainer_email "adam@hipsnip.com/remy@hipsnip.com"
 license          "Apache 2.0"
 description      "Installs/Configures Jetty"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'), :encoding => 'utf-8')
 version          "0.9.0"
 
 supports 'ubuntu', ">= 12.04"
