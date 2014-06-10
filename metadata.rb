4aa72b6b43b1c3420ff0f735049d4744a72a9447
Joseph Chen <joe1chen@hotmail.com>
2014-06-09 22:02:37 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "adam@hipsnip.com/remy@hipsnip.com"
 license          "Apache 2.0"
 description      "Installs/Configures Jetty"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'), :encoding => 'utf-8')
-version          "0.9.0"
+version          "0.9.0.1"
 
 supports 'ubuntu', ">= 12.04"
 %w{ centos redhat fedora }.each do |os|
